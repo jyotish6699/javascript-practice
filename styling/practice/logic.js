2badf29f97723a6de3d47b4b72c51930758ac93b
jyotish <jyotishkumar725015@gmail.com>
2025-11-17 08:20:16 +0000

    first commit

---
+++ styling/practice/logic.js
@@ -0,0 +1,18 @@
+// //for loop
+// //initilization=>condition=>execution=>increment/decrement
+// var j=0;
+// for(var j=0;j<5;j++){
+//     console.log("hello jyotish");
+// }
+// //while loop
+// var k=0;
+// while(k<5){
+//     console.log("hello jyotish");
+//     k++;
+// }
+
+//make a table of user given number
+let k=Number(prompt("Enter a number to print its table:"));
+for(var i=1;i<=10;i++){
+    console.log(k*i);
+}
