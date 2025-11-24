75fd2c0be901808019dc709678f1d1084ed2741b
jyotish <jyotishkumar725015@gmail.com>
2025-11-24 18:52:25 +0000

    styling start

--- styling/practice/logic.js
+++ styling/practice/logic.js
@@ -4,6 +4,7 @@
 // for(var j=0;j<5;j++){
 //     console.log("hello jyotish");
 // }
+
 // //while loop
 // var k=0;
 // while(k<5){
@@ -11,8 +12,249 @@
 //     k++;
 // }
 
-//make a table of user given number
-let k=Number(prompt("Enter a number to print its table:"));
-for(var i=1;i<=10;i++){
-    console.log(k*i);
+// //do while loop
+// var m=1;
+// do{
+//     console.log(m);
+//     m++;
+// }while(m<=5);
+
+////make a table of user given number
+// let k=Number(prompt("Enter a number to print its table:"));
+// for(var i=1;i<=10;i++){
+//     console.log(k*i);
+// }
+
+
+////nested for loop
+// for(var i=1;i<=2;i++){
+//     for(var j=2;j<=3;j++){
+//         for(var k=3;k<=4;k++){
+//             if(i==1 && j==2 && k==3){
+//                 console.log("i="+i+",j="+j+",k="+k);
+//             }else if (i==1 && j==2 && k==4){
+//                 console.log("i="+(i+3)+",j="+(j+3)+",k="+(k+2));
+//             }
+            
+//         }
+//     }  
+// }
+
+////printing trianle of *
+// for(var i=1;i<=3;i++){
+//     for(var j=1;j<=2;j++){
+//         console.log("*");
+//     };
+    
+// };
+
+////post increment and decrement
+// var a=4;
+// console.log(a++);
+// console.log(a--);
+
+////pre increment and decrement
+// var b=4;
+// console.log(++b);
+// console.log(--b);
+
+////
+// var a=5;
+// var b=++a;
+// var c=a++;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+////
+// var a=5;
+// a++;
+// ++a;
+// console.log(a);
+
+////
+// var a=10;
+// var b=--a;
+// var d=a--;
+// var e=--a;
+// console.log(a);
+// console.log(b);
+// console.log(d);
+// console.log(e);
+
+// var a=10;
+// b=--a;
+// console.log(a);
+// console.log(b);
+
+////b=a-- means first assign value of a in b then decrement of value of a
+////b=--a means first decrement value of a then assign the value of a in b
+// var a=10;
+// var b=a--;
+// console.log(a);
+// console.log(b);
+
+
+
+
+////-----------------------------------------------
+////---------------------------------------------
+
+
+////Functions 
+////Types of functions 
+////(1)Named function==>function defined with name of function and called using that name
+////function to demonstrate return statement
+// function sum(a,b){
+//     return a+b;
+
+
+////function with parameters and arguments
+// function multiply(x,y){
+//     console.log(x*y);
+// }
+// multiply(4,5);
+// multiply(6,7);
+// multiply(8,9);
+
+
+////taking multiple arguments using single parameter
+////method1 using array
+// function addAll(){
+//     var sum=0;
+//     for(var i=0;i<arguments.length;i++){
+//         sum+=arguments[i];
+//     }
+//     console.log("Sum is: "+sum);
+// }
+// addAll(1,2,3);
+// addAll(4,5,6,7,8);
+// addAll(9,10);
+
+////method2 using rest operator
+// function addAllRest(...numbers){
+//     console.log(numbers);
+// }
+// addAllRest(1,2,3,4,5,"six");
+// addAllRest(6,7,8,9);
+// addAllRest(10,11);
+
+////write a program to subtact two numbers
+// var a=Number(prompt("Enter first number:"));
+// var b=Number(prompt("Enter second number:"));
+
+// function subTwoNum(e,t){
+//     console.log(e-t);
+// }
+// subTwoNum(a,b);  
+// subTwoNum(b,a);
+
+//function with default arguments
+// function greetUser(name="Guest"){
+//     console.log("Welcome:"+name+"!");
+// }
+// greetUser();
+// greetUser("jyotish");
+
+//write a program to develope a calculator using function
+
+// operator = prompt("Enter operator (+, -, *, /):");
+// num1 = parseFloat(prompt("Enter first number:"));
+// num2 = parseFloat(prompt("Enter second number:"));
+// function calculator(num1,num2,operator){
+//     switch(operator){
+//         case "+":
+//             console.log(num1+num2);
+//             break;
+//         case "-":
+//             console.log(num1-num2);
+//             break;
+//         case "*":
+//             console.log(num1*num2);
+//             break;
+//         case "/":
+//             console.log(num1/num2);
+//             break;
+//         default:
+//             console.log("Invalid operator");
+//     }
+// }
+
+
+////(2)Anonymous function==>function defined without name and called using variable name in python like lambda function
+
+// const square = function(num){
+//     return num*num;
+// }
+// console.log(square(5));
+
+////(3)Arrow function==>shorter syntax of writing function using arrow notation
+// const add = ()=>{
+//     return "Hello jyotish";
+// }
+// console.log(add());
+
+////(4)Immediately Invoked Function Expression(IIFE) ==>function which is invoked immediately after its creation
+// (function(){
+//     console.log("IIFE executed");
+// })
+// ();
+
+////(5)Callback function ==>function passed as argument to another function and called inside that function
+function add(x,y,callback){
+    var sum=x+y;
+    callback(sum);
 }
+function displayResult(result){
+    console.log("Result is: "+result);
+}
+add(5,10,displayResult);
+
+
+
+
+
+
+
+
+
+
+
+
+////-------------------------------------
+////classes and object
+////---------------
+////Creation using object literals
+// var obj={
+//     name:"jyotish",
+//     college:"LPU",
+//     state:"Bihar"
+// };
+// console.log(obj.name);
+// console.log(obj.college);
+// console.log(obj.state);
+
+////Creation using  new object() constructor
+////js object is mutable
+////js is case-sensitive
+
+
+    
+// var obj=new Object();
+// obj.name="Rahul"
+// obj.age=20
+// obj.Roll_no=21
+
+//console.log(obj.name);
+////delete property of object using delete operator
+//delete obj.name;
+////access using square notation
+//console.log(obj["name"]);
+////access using dot notation
+//console.log(obj.name);
+////check existance or not
+//console.log("Jyotish" in obj);
+//console.log(obj.hasOwnProperty("Rahul"));
+
+//
+
